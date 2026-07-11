97649e6985eef44cbebf24f66d282a9953dc1816
GunnamKarthik <145652156+GunnamKarthik@users.noreply.github.com>
2026-07-11 08:35:11 +0100

    Add vendor POST route to create new vendors
    https://flask.palletsprojects.com/en/stable/quickstart/#http-methods

--- routes_vendors.py
+++ routes_vendors.py
@@ -18,3 +18,27 @@ def get_all_vendors():
     # Convert each row to a dictionary so it can be returned as JSON
     result = [dict(row) for row in vendors]
     return jsonify(result)
+
+
+@vendors_bp.route('/api/vendors', methods=['POST'])
+def create_vendor():
+    """Create a new vendor record."""
+    data = request.get_json()
+
+    # Check that the required field is provided
+    if not data.get('company_name'):
+        return jsonify({'error': 'Company name is required'}), 400
+
+    db = get_db()
+    # Insert the new vendor into the database
+    cursor = db.execute(
+        'INSERT INTO vendors (company_name, contact_email, website, country, default_currency) VALUES (?, ?, ?, ?, ?)',
+        (data['company_name'], data.get('contact_email', ''), data.get('website', ''),
+         data.get('country', ''), data.get('default_currency', 'USD'))
+    )
+    db.commit()
+
+    # Fetch the newly created vendor to return it
+    new_vendor = db.execute('SELECT * FROM vendors WHERE id = ?', (cursor.lastrowid,)).fetchone()
+    db.close()
+    return jsonify(dict(new_vendor)), 201
